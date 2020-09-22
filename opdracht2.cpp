609d15179b259ea8a11c422fafbcd1e568b94b4f
Walt Duivenvoorde <waltd@walt-pc>
2020-09-22 12:57:04 +0200

    opdracht2

--- opdracht2.cpp
+++ opdracht2.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 #include <fstream>
 #include <climits>
+#include <cmath>
+
+using namespace std;
+
+bool palindrome(int *p, int len) {
+	int locLen = len - 1;
+	int numb = 0;
+	int revNumb = 0;
+	for (int i = 0; i < len; i++) {
+		numb += *(p + i) * pow(10, locLen);
+		locLen--;
+	}
+	for (int i = len - 1; i >= 0; i--) {
+		revNumb += *(p + i) * pow(10, i);
+	}
+	if (numb == revNumb) {
+		return true;
+	}
+	else
+		return false;
+}
 
 int main() {
+	int	locGetal, getal;
+	int len = 0;
+	cout << "Vul een getal in: \n";
+	cin >> locGetal;
+	getal = locGetal;
+	while(locGetal > 0) {
+		locGetal /= 10;
+		len++;
+	}
+	int arr[len] = {};
+	for (int i = len - 1; i >= 0; i--){
+		arr[i] = getal % 10; 
+		getal /= 10;
+	}
+	int *p = arr;
+	if (palindrome(p, len)) {
+		cout << "True\n";
+	}
+	else
+		cout << "False\n";
 	return 0;
 }
