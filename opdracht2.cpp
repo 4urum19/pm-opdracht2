02ab95c6e96636bfa07780c5ec70829641c5b192
Naitzirch <naitzirch@gmail.com>
2020-09-25 15:49:40 +0200

    Merge

--- opdracht2.cpp
+++ opdracht2.cpp
@@ -1,9 +1,12 @@
 /*lajdlf;kasjdf;laksjdf */
+
 #include <iostream>
 #include <fstream>
 #include <climits>
 #include <cmath>
 
+#include <cstdlib> //voor exit
+#include <string>
 using namespace std;
 
 void lenCalc(int getal, int & len) {
@@ -20,6 +23,23 @@ int * numConv(int getal, int len, int arr[]) {
 		getal /= 10;
 	}
 	return arr;
+void input(string inputfile){ //functie voor de input en output van een file
+        ifstream invoer;
+        ofstream uitvoer;
+        char kar;
+        invoer.open (inputfile,ios::in); // koppel invoer aan file
+        if ( ! invoer ) {
+            cout << "File niet geopend" << endl;
+            exit (1); // stopt het programma;
+        } // if
+        uitvoer.open ("uitvoer.txt",ios::out);
+        kar = invoer.get ( );
+        while ( ! invoer.eof ( ) ) {
+            uitvoer.put (kar);
+            kar = invoer.get ( );
+        } // while
+        invoer.close ( );
+        uitvoer.close ( );
 }
 
 bool palindrome(int *p, int len) {
@@ -39,6 +59,10 @@ bool palindrome(int *p, int len) {
 	else
 		return false;
 }
+int main() {
+string inputfile = "invoer.txt";
+input(inputfile);
+return 0;
 
 bool lychrelNumb(int *p, int getal, int len) {
 	int origgetal = getal;
