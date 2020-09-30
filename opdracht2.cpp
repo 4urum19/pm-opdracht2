2d13ad02a3488493f54f0504e67ae5770ed50ab6
Walt Duivenvoorde <waltd@walt-pc>
2020-09-30 14:36:27 +0200

    opdracht2

--- opdracht2.cpp
+++ opdracht2.cpp
@@ -2,25 +2,32 @@
 #include <iostream>
 #include <fstream>
 #include <climits>
-#include <cmath>
 
 using namespace std;
+
+int pow(int power, int numb) {
+	int retPow = 1;
+	for (int i = 0; i < numb; i++) {
+		retPow *= power;
+	}
+	return retPow;
+}
 
 void lenCalc(int getal, int & len) {
 	while (getal > 0) {
 		getal /= 10;
 		len++;
-	}
-}
+	} //while
+} //lenCalc
 
 int * numConv(int getal, int len, int arr[]) {
 	int locGetal = getal;
 	for (int i = len - 1; i >= 0; i--) {
 		arr[i] = getal % 10;
 		getal /= 10;
-	}
+	} //for
 	return arr;
-}
+} //numConv
 
 bool palindrome(int *p, int len) {
 	int locLen = len - 1;
@@ -29,16 +36,17 @@ bool palindrome(int *p, int len) {
 	for (int i = 0; i < len; i++) {
 		numb += *(p + i) * pow(10, locLen);
 		locLen--;
-	}
+	} //for
 	for (int i = len - 1; i >= 0; i--) {
 		revNumb += *(p + i) * pow(10, i);
-	}
+	} //for
 	if (numb == revNumb) {
 		return true;
-	}
-	else
+	} //if
+	else {
 		return false;
-}
+	} //else
+} //palindrome
 
 bool lychrelNumb(int *p, int getal, int len) {
 	int origgetal = getal;
@@ -50,35 +58,30 @@ bool lychrelNumb(int *p, int getal, int len) {
 		int revNumb = 0;
 		for (int i = len - 1; i >= 0; i--) {
 			revNumb += *(p + i) * pow(10, i);
-		}
+		} //for
 		if (palindrome(p, len)) {
 			cout << origgetal << " is geen lychrelgetal\n"
 				 << "Dit werd duidelijk na " << it << " iteraties\n";
 			return true;
 			break;
-		}
-		else if (revNumb > 0){
-			std::cout << getal << " + " << revNumb << " = ";
-			getal += revNumb;
-			std::cout << getal << '\n';
-			if (getal > 0) {
-				len = 0;
-				lenCalc(getal, len);
-				p = numConv(getal, len, arr);
-				it++;
-			}
-			else {
-				break;
-			}
-		}
+		} //if
+		std::cout << getal << " + " << revNumb << " = ";
+		getal += revNumb;
+		std::cout << getal << '\n';
+		if (getal > 0 && getal < 100000000) {
+			len = 0;
+			lenCalc(getal, len);
+			p = numConv(getal, len, arr);
+			it++;
+		} //if
 		else {
 			break;
-		}
-	}
+		} //else
+	} //while
 	cout << origgetal << " is een lychrelkandidaat\n"
 		 << "Omdat er na " << it << " INT_MAX werd bereikt\n";
 	return false;
-}
+} //lychrelNumb
 
 int main() {
 	int getal, *p;
@@ -87,4 +90,4 @@ int main() {
 	cin >> getal;
 	lychrelNumb(p, getal, len);
 	return 0;
-}
+} //main
